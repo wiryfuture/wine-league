5e7d87b1da293e12cd9812410a0036540302eb4c
Philip Wilk <wiryfuture@gmail.com>
2021-12-15 20:26:23 +0000

    alternate patch

--- league-patches.sh
+++ league-patches.sh
@@ -25,7 +25,7 @@ cd /builddir/wine && patch -Np1 < /builddir/patches/wine/hotfix-iphlpapi-212361.
 #cd /builddir/wine && patch -Np1 < /builddir/patches/wine/hotfix-bf4_ping.patch
 
 # Frogging patch
-cd /builddir/wine && patch -Np1 < /builddir/patches/proton/childwindow.patch
+cd /builddir/wine && patch -Np1 < /builddir/wine/proton/childwindow.patch
 
 # Apply GE's league patches
 # cd /builddir/wine && patch -Np1 < /builddir/patches/lol/
