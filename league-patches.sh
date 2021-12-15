d50a5414767a4d48f8f5751478246f5d0c40da64
Philip Wilk <wiryfuture@gmail.com>
2021-12-15 18:22:40 +0000

    idk

--- league-patches.sh
+++ league-patches.sh
@@ -12,7 +12,10 @@
 cd /builddir/wine && patch -Np1 < /builddir/patches/wine/imm32-com-initialization_no_net_active_window.patch
 cd /builddir/wine && patch -Np1 < /builddir/patches/proton/01-proton-use_clock_monotonic.patch
 cd /builddir/wine && patch -Np1 < /builddir/patches/proton/04-proton-LAA_staging.patch
+# fsync esync patches
+#cd /builddir/wine && patch -Np1 < /builddir/patches/proton/esync-unix-mainline.patch
 cd /builddir/wine && patch -Np1 < /builddir/patches/proton/03-proton-fsync_staging.patch
+
 cd /builddir/wine && patch -Np1 < /builddir/patches/proton/40-proton-futex2.patch
 cd /builddir/wine && patch -Np1 < /builddir/patches/proton/49-proton_QPC.patch
 cd /builddir/wine && patch -Np1 < /builddir/patches/lol/lfh-non-proton-pre-needed.patch
@@ -21,6 +24,9 @@ cd /builddir/wine && patch -Np1 < /builddir/patches/wine/hotfix-winelib.patch
 cd /builddir/wine && patch -Np1 < /builddir/patches/wine/hotfix-iphlpapi-212361.patch
 cd /builddir/wine && patch -Np1 < /builddir/patches/wine/hotfix-ntdll-socket-212770.patch
 cd /builddir/wine && patch -Np1 < /builddir/patches/wine/hotfix-bf4_ping.patch
+
+# Frogging patch
+cd /builddir/wine && patch -Np1 < /builddir/patches/proton/childwindow.patch
 
 # Apply GE's league patches
 # cd /builddir/wine && patch -Np1 < /builddir/patches/lol/
@@ -34,7 +40,7 @@ cd /builddir/wine && autoreconf -f
 # Apply my patches (testing)
 # cd /builddir/wine && patch -Np1 < /builddir/patches/
 #cd /builddir/wine && patch -Np1 < /builddir/patches/0001-attempt-isb-bodge.patch
-#cd /builddir/wine && patch -Np1 < /builddir/patches/0003-this-is-wrong-but-what-if-we-just-ignore-this-lol.patch
+cd /builddir/wine && patch -Np1 < /builddir/patches/0003-this-is-wrong-but-what-if-we-just-ignore-this-lol.patch
 #cd /builddir/wine && patch -Np1 < /builddir/patches/0001-implement-timeout-on-GetAddrInfoExW.patch
 cd /builddir/wine && patch -Np1 < /builddir/patches/0001-remove-ret-values-from-getaddrinfoexw-so-handle-happ.patch
 cd /builddir/wine && patch -Np1 < /builddir/patches/0001-possible-query-wire-only-hotfix.patch
