16cb8c34a9a43b82e4b9f7dd4430b8486895fa2c
Philip Wilk <wiryfuture@gmail.com>
2021-12-19 23:21:26 +0000

    should be a clone of GE's league build

--- league-patches.sh
+++ league-patches.sh
@@ -15,20 +15,16 @@ cd /builddir/wine && patch -Np1 < /builddir/patches/proton/01-proton-use_clock_m
 cd /builddir/wine && patch -Np1 < /builddir/patches/proton/LAA-unix-staging.patch
 
 # fsync esync patches
-#cd /builddir/wine && patch -Np1 < /builddir/patches/proton/esync-unix-mainline.patch
-#cd /builddir/wine && patch -Np1 < /builddir/patches/proton/03-proton-fsync_staging.patch
+cd /builddir/wine && patch -Np1 < /builddir/patches/proton/esync-unix-mainline.patch
 
-#cd /builddir/wine && patch -Np1 < /builddir/patches/proton/40-proton-futex2.patch
-#cd /builddir/wine && patch -Np1 < /builddir/patches/proton/49-proton_QPC.patch
+cd /builddir/wine && patch -Np1 < /builddir/patches/proton/40-proton-futex2.patch
+cd /builddir/wine && patch -Np1 < /builddir/patches/proton/49-proton_QPC.patch
 cd /builddir/wine && patch -Np1 < /builddir/patches/lol/lfh-non-proton-pre-needed.patch
-#cd /builddir/wine && patch -Np1 < /builddir/patches/proton/50-proton_LFH.patch
-#cd /builddir/wine && patch -Np1 < /builddir/patches/wine/hotfix-winelib.patch
+cd /builddir/wine && patch -Np1 < /builddir/patches/proton/50-proton_LFH.patch
+cd /builddir/wine && patch -Np1 < /builddir/patches/wine/hotfix-winelib.patch
 cd /builddir/wine && patch -Np1 < /builddir/patches/wine/hotfix-iphlpapi-212361.patch
-##cd /builddir/wine && patch -Np1 < /builddir/patches/wine/hotfix-ntdll-socket-212770.patch
-#cd /builddir/wine && patch -Np1 < /builddir/patches/wine/hotfix-bf4_ping.patch
-
-# Frogging patch
-#cd /builddir/wine && patch -Np1 < /builddir/patches/wine/childwindow.patch
+cd /builddir/wine && patch -Np1 < /builddir/patches/wine/hotfix-ntdll-socket-212770.patch
+cd /builddir/wine && patch -Np1 < /builddir/patches/wine/hotfix-bf4_ping.patch
 
 # Apply GE's league patches
 # cd /builddir/wine && patch -Np1 < /builddir/patches/lol/
@@ -38,14 +34,3 @@ cd /builddir/wine && patch -Np1 < /builddir/patches/lol/0001-bodge-old-patch.pat
 /builddir/wine/dlls/winevulkan/make_vulkan
 /builddir/wine/tools/make_requests
 cd /builddir/wine && autoreconf -f
-
-# Apply my patches (testing)
-# cd /builddir/wine && patch -Np1 < /builddir/patches/
-#cd /builddir/wine && patch -Np1 < /builddir/patches/0001-attempt-isb-bodge.patch
-#cd /builddir/wine && patch -Np1 < /builddir/patches/0003-this-is-wrong-but-what-if-we-just-ignore-this-lol.patch
-##cd /builddir/wine && patch -Np1 < /builddir/patches/0001-implement-timeout-on-GetAddrInfoExW.patch
-#cd /builddir/wine && patch -Np1 < /builddir/patches/0001-remove-ret-values-from-getaddrinfoexw-so-handle-happ.patch
-#cd /builddir/wine && patch -Np1 < /builddir/patches/0001-possible-query-wire-only-hotfix.patch
-
-# Reverse some scuffed patch to master
-#cd /builddir/wine && patch -Np1 < /builddir/patches/wine/0001-commit-737fe1f-is-dead-to-me.patch
