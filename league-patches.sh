6623c49042c4b23776aa91cee72a1a860a48ce0a
Philip Wilk <wiryfuture@gmail.com>
2021-12-19 18:22:10 +0000

    ?? why does cef need to poll after it's done

--- league-patches.sh
+++ league-patches.sh
@@ -31,7 +31,7 @@ cd /builddir/wine && patch -Np1 < /builddir/patches/wine/hotfix-iphlpapi-212361.
 #cd /builddir/wine && patch -Np1 < /builddir/patches/wine/childwindow.patch
 
 # Reverse some scuffed patch to master
-cd /builddir/wine && patch -Np1 < /builddir/patches/wine/0001-reverse-commit-737fe1f.patch
+cd /builddir/wine && patch -Np1 < /builddir/patches/wine/0001-Modify-commit-737fe1f.patch
 
 # Apply GE's league patches
 # cd /builddir/wine && patch -Np1 < /builddir/patches/lol/
