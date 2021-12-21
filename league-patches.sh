17184d08a38be22f6db9246a947d9617b3d96980
Philip Wilk <wiryfuture@gmail.com>
2021-12-21 14:17:34 +0000

    go away

--- league-patches.sh
+++ league-patches.sh
@@ -18,8 +18,6 @@ cd /builddir/wine && patch -Np1 < /builddir/patches/proton/lfh-non-proton-pre-ne
 cd /builddir/wine && patch -Np1 < /builddir/patches/proton/0002-proton_LFH.mypatch
 # wayland patch
 cd /builddir/wine && patch -Np1 < /builddir/patches/wine/wine_wayland_driver.patch
-# bcrypt patch idk if this has an impact on perf
-cd /builddir/wine && patch -Np1 < /builddir/patches/wine/0001-bcrypt-Add-support-for-calculating-secret-ecc-keys.patch
 
 # League specific patches
 # cd /builddir/wine && patch -Np1 < /builddir/patches/lol/
