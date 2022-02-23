004fc388d93a35243c3fb3ab47fc4849c1ad4145
Philip Wilk <wiryfuture@gmail.com>
2022-02-23 15:23:41 +0000

    surely

--- scripts/buildme/build.sh
+++ scripts/buildme/build.sh
@@ -22,9 +22,9 @@ PKGTYPE=${PKGTYPE:="-R"}
 ####################################
 # 64 bit build process (includes 32 bit. will take about twice the time because.)
 cd /builddir/out64 && CC="ccache gcc" ../wine/configure --enable-win64 && make -j"$THREADS"
-cat /builddir/wine/config.log
+cat /builddir/out64/config.log
 cd /builddir/out32 && PKG_CONFIG_PATH=/usr/lib/pkgconfig CC="ccache gcc -m32" ../wine/configure --with-wine64=../out64 && make -j"$THREADS"
-cat /builddir/wine/config.log
+cat /builddir/out32/config.log
 # create packages
 cd /builddir/out32 && checkinstall --install=no --pkgname="wine" --pkgarch="i686" --provides="wine" --pakdir=/exports/out32 $PKGTYPE
 cd /builddir/out64 && checkinstall --install=no --pkgname="wine" --pkgarch="x86_64" --provides="wine" --pakdir=/exports/out64 $PKGTYPE
